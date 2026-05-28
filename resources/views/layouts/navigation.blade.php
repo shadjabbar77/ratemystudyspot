6d13a206cc6c81f11986fd5e113718e653ff316d
“Shad <shadjay@students.cs.ubc.ca>
2026-05-27 18:51:41 -0700

    Replace logo with black star

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-               <!-- Logo Star -->
+            <!-- Logo Star -->
 <div class="shrink-0 flex items-center">
     <a href="{{ route('study-spots.index') }}"
        class="text-black text-3xl font-bold leading-none"
