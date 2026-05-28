a4b29565a5336cb3fd24186c77663f4dab925e3b
“Shad <shadjay@students.cs.ubc.ca>
2026-05-27 18:48:40 -0700

    Replace logo with black star

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,12 +12,14 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+               <!-- Logo Star -->
+<div class="shrink-0 flex items-center">
+    <a href="{{ route('study-spots.index') }}"
+       class="text-black text-3xl font-bold leading-none"
+       aria-label="Rate My Study Spot Home">
+        ★
+    </a>
+</div>
 
 <div class="hidden space-x-12 sm:-my-px sm:ms-10 sm:flex">
 </div>
